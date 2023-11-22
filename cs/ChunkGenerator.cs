5eea00ab88d007d4aedf03a123dfd569916372b7
pkhead <chromosoze@gmail.com>
2023-11-22 00:07:44 -0600

    terrain generation

--- cs/ChunkGenerator.cs
+++ cs/ChunkGenerator.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.Collections;
 using System.Collections.Generic;
 
 public partial class ChunkGenerator : Node3D
@@ -9,7 +10,7 @@ public partial class ChunkGenerator : Node3D
 
 	private class ChunkData
 	{
-		private int[] data;
+		private readonly int[] data;
 
 		public ChunkData()
 		{
@@ -36,39 +37,97 @@ public partial class ChunkGenerator : Node3D
 		}
 	}
 
+	private int _chunkX = 0;
+	private double _accum = 0.0f;
+	private FastNoiseLite noise;
+
+	private readonly Dictionary<uint, ChunkData> chunks = new();
+
+	private static uint Pair2u(uint x, uint y) => 
+		x >= y ? x*x + x + y : y*y + x;
+
+	private static uint Pair2s(int x, int y) =>
+		Pair2u(
+			x >= 0 ? (uint)(x*2) : (uint)(-x*2+1),
+			y >= 0 ? (uint)(y*2) : (uint)(-y*2+1)
+		);
+
+	private void AddChunk(int cx, int cz, ChunkData chunk)
+	{
+		chunks[Pair2s(cx, cz)] = chunk;
+	}
+
+	private ChunkData GetChunk(int cx, int cz)
+	{
+		return chunks[Pair2s(cx, cz)];
+	}
+
+	private bool TryGetChunk(int cx, int cz, out ChunkData chunk)
+	{
+		return chunks.TryGetValue(Pair2s(cx, cz), out chunk);
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		// generate chunk data
-		var chunkData = new ChunkData();
+		noise = new() {
+			NoiseType = FastNoiseLite.NoiseTypeEnum.Simplex,
+		};
 
-		var prevTime = Time.GetTicksMsec();
-
-		for (int x = 0; x < CHUNK_WIDTH; x++)
+		for (int x = 0; x < 10; x++)
 		{
-			for (int y = 0; y < CHUNK_HEIGHT; y++)
+			for (int z = 0; z < 10; z++)
 			{
-				for (int z = 0; z < CHUNK_DEPTH; z++)
-				{
-					chunkData.Set(x, y, z, y < 64 ? 1 : 0);
-				}
+				GenerateChunk(x, z);
 			}
 		}
 
-		GD.Print($"chunk gen took {(Time.GetTicksMsec() - prevTime)} ms");
-
-		MeshChunk(chunkData);
+		for (int x = 0; x < 10; x++)
+		{
+			for (int z = 0; z < 10; z++)
+			{
+				MeshChunk(x, z);
+			}
+		}
 	}
 
-	// Return true if a given block id is transparent
-	private static bool IsTransparent(int blockId)
+	private void GenerateChunk(int cx, int cz)
+	{
+		// if chunk at (cx, cz) already exists, do not regenerate chunk
+		if (TryGetChunk(cx, cz, out _)) return;
+		var prevTime = Time.GetTicksMsec();
+
+		var chunkData = new ChunkData();
+
+		for (int x = 0; x < CHUNK_WIDTH; x++)
+		{
+			for (int z = 0; z < CHUNK_DEPTH; z++)
+			{
+				int height = (int)(noise.GetNoise2D(x + cx * CHUNK_WIDTH, z + cz * CHUNK_DEPTH) * 20) + 64;
+
+				for (int y = 0; y < CHUNK_HEIGHT; y++)
+				{
+					chunkData.Set(x, y, z, y >= height ? 0 : 1);
+				}
+			}
+		}
+		
+		AddChunk(cx, cz, chunkData);
+
+		GD.Print($"chunk gen took {(Time.GetTicksMsec() - prevTime)} ms");
+	}
+
+    // Return true if a given block id is transparent
+    private static bool IsTransparent(int blockId)
 	{
 		return blockId == 0;
 	}
 
-	private void MeshChunk(ChunkData chunkData)
+	private void MeshChunk(int cx, int cz)
 	{
 		var startTime = Time.GetTicksMsec();
+
+		var chunkData = GetChunk(cx, cz);
 
 		var surfaceArray = new Godot.Collections.Array();
 		surfaceArray.Resize((int)Mesh.ArrayType.Max);
@@ -125,7 +184,7 @@ public partial class ChunkGenerator : Node3D
 					// air is invisible
 					if (chunkData.Get(x, y, z) == 0) continue;
 
-					Vector3I blockPos = new(x, y, z);
+					Vector3I blockPos = new(x + cx * CHUNK_WIDTH, y, z + cz * CHUNK_DEPTH);
 
 					// top face
 					if (IsTransparent(chunkData.Get(x, y+1, z)))
@@ -191,6 +250,8 @@ public partial class ChunkGenerator : Node3D
 		}
 
 		// convert lists to arrays and assign to surface array
+		GD.Print(verts.Count);
+
 		surfaceArray[(int)Mesh.ArrayType.Vertex] = verts.ToArray();
 		surfaceArray[(int)Mesh.ArrayType.TexUV] = uvs.ToArray();
 		surfaceArray[(int)Mesh.ArrayType.Normal] = normals.ToArray();
@@ -199,7 +260,7 @@ public partial class ChunkGenerator : Node3D
 		// finalize mesh
 		var arrayMesh = new ArrayMesh();
 		arrayMesh.AddSurfaceFromArrays(Mesh.PrimitiveType.Triangles, surfaceArray);
-		GD.Print($"chunk gen took {Time.GetTicksMsec() - startTime} ms");
+		GD.Print($"chunk mesh took {Time.GetTicksMsec() - startTime} ms");
 
 		var material = new StandardMaterial3D() {
 			AlbedoTexture = GD.Load("res://stone.png") as Texture2D,
